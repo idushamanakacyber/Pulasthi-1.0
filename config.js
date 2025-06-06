816d4392b2abe160073b8b18bdcb3c6b0f6dab76
Idusha Manaka <127433800+idushamanakacyber@users.noreply.github.com>
2025-06-07 00:41:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.AIVE_IMG || "https://avatars.githubusercontent.com/u/127433800?v=4",
+ALIVE_MSG: process.env.AIVE_MSG || "Hello, I am Pulasthi 1.0 WhatsApp bot I am alive now!",
 };
